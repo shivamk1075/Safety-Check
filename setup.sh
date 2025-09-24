796fe93922f68dcf40bc943f79818b34b6fe3542
Shivam Kumar <shivam.kumar.101075@gmail.com>
2025-09-24 22:36:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
-# #!/bin/bash
-# pip install streamlit-folium==0.22.1
-
 #!/bin/bash
+pip install streamlit-folium==0.22.1
 
-# Exit immediately if a command fails
-set -e
+# #!/bin/bash
 
-# Update package list
-apt-get update -y
+# # Exit immediately if a command fails
+# set -e
 
-# Remove conflicting packages that are pre-installed on the base image
-apt-get remove -y libodbc1 odbcinst odbcinst1debian2 && apt-get autoremove -y
+# # Update package list
+# apt-get update -y
 
-# Install all packages listed in packages.txt
-xargs -a packages.txt apt-get install -y
+# # Remove conflicting packages that are pre-installed on the base image
+# apt-get remove -y libodbc1 odbcinst odbcinst1debian2 && apt-get autoremove -y
+
+# # Install all packages listed in packages.txt
+# xargs -a packages.txt apt-get install -y
